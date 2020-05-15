42a94998e5ce8cb8b025e60ed7bfdb78b2bd0f45
Julien Lacour <julienlacour31@gmail.com>
2020-05-15 16:25:58 +0300

    Create Letter component and remove comments

---
+++ src/Letter.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Letter = ({ letter, usedLetters, onClick }) => (
+  <button
+    disabled={usedLetters.has(letter)}
+    key={letter}
+    onClick={() => onClick(letter)}
+  >
+    {letter}
+  </button>
+);
+
+export default Letter
